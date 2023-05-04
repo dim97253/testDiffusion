36510b1125040de54b460e760ce75c4cb718a892
dim97253 <123586859+dim97253@users.noreply.github.com>
2023-05-04 13:07:04 +0300

    Add files via upload

---
+++ preloadModels.py
@@ -0,0 +1,23 @@
+from flask import Flask, send_file, request, render_template, make_response
+from diffusers import DiffusionPipeline, StableDiffusionPipeline, DPMSolverMultistepScheduler, StableDiffusionPipeline, EulerAncestralDiscreteScheduler, AutoencoderKL, HeunDiscreteScheduler, StableDiffusionImg2ImgPipeline, StableDiffusionInpaintPipeline
+import torch
+import sys # to access the system
+import base64
+from PIL import Image
+import io
+from io import BytesIO
+
+models = []
+models.append('prompthero/openjourney-v2')
+models.append('runwayml/stable-diffusion-v1-5')
+models.append('andite/anything-v4.0')
+models.append('stabilityai/stable-diffusion-2-inpainting')
+models.append('stabilityai/stable-diffusion-x4-upscaler')
+
+for model in models :
+    print ('loading '+str(model))
+    StableDiffusionPipeline.from_pretrained(model)
+
+print ('all models downloaded')
+
+

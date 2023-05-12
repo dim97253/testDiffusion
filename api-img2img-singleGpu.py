63244252d6fc1e6de04d27c1fcc2e5ea0e01a7c2
dim97253 <123586859+dim97253@users.noreply.github.com>
2023-05-12 03:06:18 +0300

    Update api-img2img-singleGpu.py

--- api-img2img-singleGpu.py
+++ api-img2img-singleGpu.py
@@ -7,68 +7,51 @@ from PIL import Image
 import io
 from io import BytesIO
 
-
 app = Flask(__name__)
-repo_anything = "andite/anything-v4.0"
-repo_openjourney = "prompthero/openjourney-v2"
-repo_diffusion15 = "runwayml/stable-diffusion-v1-5"
 
 def safety_checker(images, clip_input):
     return images, False
 
+repo = "andite/anything-v4.0"
+#repo_openjourney = "prompthero/openjourney-v4"
+#repo_diffusion15 = "runwayml/stable-diffusion-v1-5"
+
+pipes = []
+pipes.append(StableDiffusionImg2ImgPipeline.from_pretrained(repo))
+pipes.append(StableDiffusionImg2ImgPipeline.from_pretrained(repo))
+pipes.append(StableDiffusionImg2ImgPipeline.from_pretrained(repo))
+pipes.append(StableDiffusionImg2ImgPipeline.from_pretrained(repo))
+
+pipe_statuses=[]
+
+for i, pipe in enumerate(pipes):
+    pipe.safety_checker = safety_checker
+    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    pipe.to('cuda:'+str(i))
+    pipe_statuses.append(0) # 0 means free. 1 means busy
 
 @app.route('/', methods=['POST'])
 def img():
     data = request.get_json()
     prompt = data.get('prompt', '')
     negative_prompt = data.get('neg_prompt', '')
-    model_name = data.get('model', '')
-    steps = int(data.get('steps', ''))
     src_img64 = str(data.get('srcimage', ''))
-    if src_img64 != 'undefined':
-        pipe = StableDiffusionImg2ImgPipeline.from_pretrained(repo_openjourney,local_files_only=True)
-        if model_name == "openjourney":
-            pipe = StableDiffusionImg2ImgPipeline.from_pretrained(repo_openjourney,local_files_only=True)
-        if model_name == "anything4":
-            pipe = StableDiffusionImg2ImgPipeline.from_pretrained(repo_anything,local_files_only=True)
-        if model_name == "diffusion15":
-            pipe = StableDiffusionImg2ImgPipeline.from_pretrained(repo_diffusion15,local_files_only=True)
-        pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-        pipe = pipe.to('cuda:0')
-        pipe.enable_attention_slicing()    
-        pipe.safety_checker = safety_checker
-        src_img = Image.open(io.BytesIO(base64.decodebytes(bytes(src_img64, "utf-8"))))
-        #images = pipe(prompt = prompt, image=src_img, strength=0.60, guidance_scale=18.0, negative_prompt = negative_prompt).images #Img2Img anything
-        images = pipe(prompt = prompt, image=src_img, strength=0.60, guidance_scale=18.0, negative_prompt = negative_prompt).images #Img2Img openjourney
-    else:
-        pipe = StableDiffusionPipeline.from_pretrained(repo_openjourney,local_files_only=True)
-        if model_name == "openjourney":
-            pipe = StableDiffusionPipeline.from_pretrained(repo_openjourney,local_files_only=True)
-        if model_name == "anything4":
-            pipe = StableDiffusionPipeline.from_pretrained(repo_anything,local_files_only=True)
-        if model_name == "diffusion15":
-            pipe = StableDiffusionPipeline.from_pretrained(repo_diffusion15,local_files_only=True)
-        pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-        pipe = pipe.to('cuda:0')
-        pipe.enable_attention_slicing()    
-        pipe.safety_checker = safety_checker
-        images = pipe(prompt = prompt, negative_prompt  = negative_prompt, num_inference_steps=steps, width =512, height=512).images
-
-        
-
-    
-    
-    image=images[0]
-
-    buffer = BytesIO()
-    image.save(buffer, format='png')
-    img_bytes = buffer.getvalue()
-    img_b64 = base64.b64encode(img_bytes)
-    #response = make_response(img_bytes)
-    #response.headers.set('Content-Type', 'image/pmg')
-    #response.headers.set(
-    #   'Content-Disposition', 'attachment', filename='output.png')
-    return img_b64
+    strength = float(data.get('strength', ''))
+    guidance_scale = float(data.get('guidance_scale', ''))
+    src_img = Image.open(io.BytesIO(base64.decodebytes(bytes(src_img64, "utf-8"))))
+    src_img = src_img.resize((512, 512))
+    for i, pipe in enumerate(pipes):
+        if pipe_statuses[i]==0:
+            pipe_statuses[i]=1
+            images = pipe(prompt = prompt, image=src_img, strength=strength, guidance_scale=guidance_scale, negative_prompt = negative_prompt).images
+            image=images[0]
+            pipe_statuses[i]=0
+            buffer = BytesIO()
+            image.save(buffer, format='jpeg')
+            img_bytes = buffer.getvalue()
+            img_b64 = base64.b64encode(img_bytes)
+            return img_b64
+    return NULL    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
